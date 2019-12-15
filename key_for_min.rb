b5baae209a199b22bae7e61aed32d59403429dc6
Alex Engelhart <asengelhart@yahoo.com>
2019-12-15 13:33:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
   name_hash.reduce("") do |memo, (key, value)|
     if !name_hash[memo] || name_hash[memo] > value 
       memo = key 
